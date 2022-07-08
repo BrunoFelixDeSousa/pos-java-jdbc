b3bd6c514885ef5800486bcad578de4504a9ee04
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-07-08 00:31:53 -0300

    add classe telefone

---
+++ src/main/java/br/com/jdev/model/Telefone.java
@@ -0,0 +1,42 @@
+package br.com.jdev.model;
+
+public class Telefone {
+
+    private Long id;
+    private String numero;
+    private String tipo;
+
+    private String usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
